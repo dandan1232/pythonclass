e9e6efd65a223715bffd22256d54de78478e4fc4
dandan1232 <345652127@qq.com>
2021-09-10 15:14:20 +0800

    字符串

---
+++ homework/printout.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/10 13:54
+# @Author  : Lindand
+# @File    : printout.py
+# @Description :
+
+# name = "小明"
+# age = 28
+# weight = 61.5
+# print("姓名为：%s,年龄为：%d,体重为：%.1f" % (name, age, weight))
+
+# a = "abcdef"
+# print(a[0:2])
+# print(a[2:5])
+# print(a[2:-1])
+# print(a[3:])
+# print(a[::2])
+# print(a[::-2])
+
+
+# print(a[0:3])
+# print(a[3:5])
+# print(a[1:-1])
+# print(a[2:])
+# print(a[::-2])
+
+# b="南京工业职业技术大学欢迎您"
+# print(b[0:2])
+# print(b[2:])
+# print(b[2:-1])
+# print(b[::-3])
+
