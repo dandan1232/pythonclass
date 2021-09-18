60a2a2bf2bdabfb833be2e18ba7f9ae5c65be56b
dandan1232 <345652127@qq.com>
2021-09-18 18:02:55 +0800

    员工薪资+列表推导式

---
+++ homework/EmployeesInput.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/18 16:13
+# @Author  : Lindand
+# @File    : EmployeesInput.py
+# @Description :
+# 需要编程录入4位员工的薪资，请按照以下要求完成程序：
+# 1、用字典或列表和字典组合记录员工的姓名和工资
+# 2、结合循环语句和else语句，录入员工的薪资；全部录入后，打印提示“您已经全部录入4名员工的薪资”
+# 3、输入员工的薪资时，若薪资小于1000则重新输入
+# 4、录入过程中，可以输入‘Q’或‘q’提前结束录入，且之前录入的薪资作废
+# 5、4位员工的薪资都正常录入，则打印出录入员工的姓名和薪资明细，以及平均薪资
+
+Sum = 0
+Count = 0
+salarys = 0
+EmpSal_list = []
+
+for i in range(4):
+    emp = input("请输入员工姓名:")
+    s = input("请输入%s的薪资(按Q或q结束):" % emp)
+    if s.upper() == 'Q' or s.upper() == 'q':
+        print("录入完成，退出")
+        break
+    if float(s) < 1000:
+        print("不可能那么少啦，请重新输入！！")
+        s = input("请重新输入:")
+        Sum += float(s)
+        EmpSal_dict = {'姓名:': emp, '薪资:': s}
+        EmpSal_list.append(EmpSal_dict)
+        Count += 1
+        continue
+    Count += 1
+    Sum += float(s)
+    EmpSal_dict = {'姓名:': emp, '薪资:': s}
+    # print(EmpSal_dict)
+    EmpSal_list.append(EmpSal_dict)
+else:
+    print("您已经全部录入4名员工的薪资:\n" + str(EmpSal_list))
+
+if (Count == 0):
+    print("录入员工数为0，已结束")
+else:
+    print("员工数:{0}".format(Count))
+    print("总工资数为：" + str(Sum))
+    print("平均薪资:{0}".format(Sum / Count))
--- homework/ListDerivaation.py
+++ homework/ListDerivaation.py
@@ -3,16 +3,22 @@
 # @Author  : Lindand
 # @File    : ListDerivaation.py
 # @Description :
-#
+# 请用列表推导式，按照要求生成列表：
+# 1、生成列表 [0, 4, 16, 36, 64]
+# 2、生成列表[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# 3、从 [‘66’, ‘007’, ‘jsy’, ‘JSY’, ‘main’] 提取出全是字母的元素生成新列表
+# 4、从 [‘66’, ‘007’, ‘jsy’, ‘JSY’, ‘main’] 生成新列表[‘66’, ‘007’, ‘JSY’, ‘jsy’, ‘MAIN’]
+# 提示：列表推导式中使用 startswith()、 upper()、lower() 等字符串函数
+
 # 写列表生成式时，把要生成的元素x * x放到前面，后面跟for循环.
 # for循环后面还可以加上if判断，这样我们就可以筛选出仅偶数的平方：
-print([x * x for x in range(1, 10) if x % 2 == 0])
+print([x * x for x in range(0, 10) if x % 2 == 0])
 
 # 通过利用列表的下标，步长和切片操作解决问题
 list5 = [i for i in range(1, 10)]
-print(list5)
+# print(list5)
 list6 = [list5[j:j + 3] for j in range(0, len(list5), 3)]
-print(len(list5))
+# print(len(list5))
 print(list6)
 
 # 列表推导式左边为输出元素，然后从左至右进行嵌套执行的顺序
@@ -22,7 +28,7 @@ list4 = [x for x in list3 if x.isalpha()]
 print(list4)
 
 # （可迭代指一个范围，使for可循环遍历）
-# list = [结果1 if 条件  else  结果2  for i in 可迭代]     （当条件语句含else时，分情况输出）
+# list = [结果1 if 条件  else  结果2  for i in 可迭代]  （当条件语句含else时，分情况输出）
 # 举例子：#如果是 J 开头的则将单词变为小写，不是 J 开头则全部大写
 # 对于列表元素的修改
 list1 = ['66', '007', 'jsy', 'JSY', 'main']
