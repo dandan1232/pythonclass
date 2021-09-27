a540cd972d1e2ee1e197225f1a043aced72e043d
dandan1232 <345652127@qq.com>
2021-09-27 10:13:58 +0800

    匿名函数三角形210927

---
+++ homework/Lamb.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/27 9:39
+# @Author  : Lindand
+# @File    : Lamb.py
+# @Description :
+
+a = float(input("a:"))
+b = float(input("b:"))
+num1 = lambda a, b: (a ** 2) + (b ** 2)
+print("两条边的平方和:" + str(num1(a, b)))
+
+c = float(input("c:"))
+if (a + b > c and a + c > b and b + c > a and abs(a - b) < c and abs(a - c) < b and abs(b - c) < a):
+    num = [a, b, c]
+    s = sorted(num, key=lambda x: x)
+    n1 = s[0]
+    n2 = s[1]
+    sum = lambda n1, n2: (n1 ** 2) + (n2 ** 2)
+    print("两条短边平方为：" + str(sum(n1, n2)))
+    if sum(n1, n2) == s[2] ** 2:
+        print("为直角三角形")
+    else:
+        print("不是直角三角形")
+else:
+    print("不能组成三角形")
