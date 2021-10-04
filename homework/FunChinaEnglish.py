bf26b1e7236959fbe71066577d5fca17d8340846
dandan1232 <345652127@qq.com>
2021-10-04 12:35:04 +0800

    请利用装饰器，在这两个函数执行的时候，分别根据其国籍，来说出一段打招呼的话，如你好!hello.211004

--- homework/FunChinaEnglish.py
+++ homework/FunChinaEnglish.py
@@ -2,24 +2,35 @@
 # @Time    : 2021/10/4 11:27
 # @Author  : Lindand
 # @File    : FunChinaEnglish.py
-# @Description :
+# @Description :有下面两个函数，分别是中国人、美国人自我介绍。
+# def chinese(): print("我来自中国。")
+# def american(): print("I am from America.")
+# 请利用装饰器，在这两个函数执行的时候，分别根据其国籍，来说出一段打招呼的话，如“你好!”“hello.”。
 
-# def say_hello():
-#     def warpper():
-#         print("你好！")
-#
-#     return warpper()
-#
-#
-# @say_hello()
-# def chinese():
-#     print("我来自中国。")
-#
-#
-# def american():
-#     print("I am from America.")
-#
-#
-# chinese()
-# print('****************')
-# american()
+def say_hello(country):
+    def wrapper(func):
+        def deco(*args, **kwargs):
+            if country == "china":
+                print("来自中国:")
+                print("你好!")
+            elif country == "america":
+                print("Come from America:")
+                print('hello!')
+            else:
+                return
+                func(*args, **kwargs)
+        return deco
+    return wrapper
+
+
+@say_hello("china")
+def chinese():
+    print("我来自中国。")
+
+@say_hello("america")
+def american():
+    print("I am from America.")
+
+chinese()
+print("——————————————————————————")
+american()
