49f461f54e5e46331f0be7af3c1084f6fc816d49
dandan1232 <345652127@qq.com>
2021-10-04 11:21:03 +0800

    用while语句从键盘输入一个数，从斐波那契数列中查找比输入数据大的最小的数，并把它输出显示

--- homework/FindFromFibonacc.py
+++ homework/FindFromFibonacc.py
@@ -2,7 +2,7 @@
 # @Time    : 2021/10/4 11:01
 # @Author  : Lindand
 # @File    : FindFromFibonacc.py
-# @Description :
+# @Description :用while语句从键盘输入一个数，从斐波那契数列中查找比输入数据大的最小的数，并把它输出显示
 
 fibon = [0, 1]
 string = input("请输入一个大于1的整数：")
@@ -11,6 +11,8 @@ while 1:
     if fibon[-1] <= number:
         fibon.append(fibon[-2] + fibon[-1])
     else:
+        print("和输入数据紧邻的三个斐波那契数列为：")
+        print(fibon[-3:])
         print("斐波那契数列中比输入数据大的最小的数是：")
         print(fibon[-1])
         break
